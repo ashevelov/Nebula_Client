7f37d1412f4518746413c1b87f01c451277f804d
shevelov <shevelev@komargames.com>
2015-05-28 13:41:03 +0300

    5

---
+++ Unity/SpaceSadomy/Assets/Shaders/AnisotropicOffset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnisotropicOffset : MonoBehaviour {
+
+	public float offsetSpeed = 1.0f;
+	private Material mat;
+
+	// Use this for initialization
+	void Start () {
+		mat = this.GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float offset = Mathf.Repeat(Time.time * offsetSpeed, 2) - 1;
+		mat.SetFloat("_AnisoOffset", offset);
+	}
+}

acbd5c21f307be5d5ab4f3e9c1bd0299f89b6b7e
shevelov <shevelev@komargames.com>
2015-05-28 15:14:25 +0300

    юнити...

---
+++ Unity/SpaceSadomy/Assets/ThirdPartyAssets/Realistic Effects Pack/Scripts/Prefabs/Waves/InstanceMaterial.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstanceMaterial : MonoBehaviour
+{
+
+  public Material Material;
+	// Use this for initialization
+	void Start ()
+	{
+    GetComponent<Renderer>().material = Material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

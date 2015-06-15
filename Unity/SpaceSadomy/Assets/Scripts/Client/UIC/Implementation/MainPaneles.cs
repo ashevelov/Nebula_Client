5a9e1e7e78cdd99b46fa9c70b2356d2e494c47be
Andrei <shevelov.andrei@gmail.com>
2015-06-15 18:58:13 +0400

    inventory

---
+++ Unity/SpaceSadomy/Assets/Scripts/Client/UIC/Implementation/MainPaneles.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainPaneles : MonoBehaviour {
+
+    public GameObject inventory;
+
+    
+}
---
+++ Unity/SpaceSadomy/Assets/Scripts/UIUtils/UUIToggleChild.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UUIToggleChild : MonoBehaviour {
+
+    public GameObject content;
+    private Toggle toggle;
+
+	void Start () {
+        toggle = GetComponent<Toggle>();
+	}
+
+    public void ChildSetActive()
+    {
+        content.SetActive(toggle.isOn);
+    }
+}
